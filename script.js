4233f4893399956eb461158e3258c883d336449e
Buz Oleksandr <oleksandr.buz@gmail.com>
2015-09-29 22:27:59 +0300

    addet new style.css file, changed script file hi now add left menu, chandet html file with new arhitect

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var menuSideBar = ['Home', 'Menu', 'Next Page', 'Search'];
 
+function leftMenu(){
+var menu = document.getElementById('menu');
 for(var i=0; i<menuSideBar.length; i++){ 
-    var menu = document.getElementById('menu');
     var div = document.createElement('div');
+    div.className = 'div div-success';
     div.innerHTML = (i+1) + ' - ' + menuSideBar[i];
     menu.appendChild(div);
-}
+    };
+};
+leftMenu();
