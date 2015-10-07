193769c5caa756df2b33d9cc3736b85bc90db056
Buz Oleksandr <oleksandr.buz@gmail.com>
2015-10-07 13:14:46 +0300

    added function to deleted list items

--- list.js
+++ list.js
@@ -7,8 +7,18 @@ var addItems = function(){
 
     var items = document.getElementById('items');
     var div = document.createElement('div');
+    var a = document.createElement('a');
+    a.innerHTML = 'delete';
+    a.href = '#';
+    a.className = "js-delete-button";
+    a.onclick = deleteItem;
 
     div.innerHTML = input.value;
+    div.appendChild(a);
     items.appendChild(div);
+}
 
+function deleteItem(event){
+    var items = document.getElementById('items');
+    items.removeChild(event.target.parentElement);
 }
